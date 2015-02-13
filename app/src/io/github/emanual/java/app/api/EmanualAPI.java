22fcf25bf2eb46e9d7833bdf77c33bfb48ddd015
Jayin Ton <tonjayin@gmail.com>
2015-02-14 00:54:04 +0800

    + Resouce Center

---
+++ app/src/io/github/emanual/java/app/api/EmanualAPI.java
@@ -0,0 +1,16 @@
+package io.github.emanual.java.app.api;
+
+import android.annotation.SuppressLint;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+public class EmanualAPI {
+	/**
+	 * 下载对应的语言
+	 * @param lang
+	 * @param responseHandler
+	 */
+	@SuppressLint("DefaultLocale") public static void downloadLang(String lang,AsyncHttpResponseHandler responseHandler){
+		RestClient.get(String.format("/md-%s/dist/%s.zip", lang.toLowerCase(),lang.toLowerCase()), null, responseHandler);
+	}
+}
